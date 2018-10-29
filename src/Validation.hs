5ea5771e778e6d905540ffecc607c9959702a7ee
EarthCitizen <ryan.neil.daniels@gmail.com>
2018-10-28 22:29:27 -0700

    Improve error message

--- src/Validation.hs
+++ src/Validation.hs
@@ -10,5 +10,6 @@ validateFnDef fd@(FnExpr fn ps ex) = forM_ ps (fNameNotPName fn) >> Right fd
 
 fNameNotPName :: Name -> Name -> Either Error Name
 fNameNotPName fn pn = if pn == fn
-                           then Left $ Error "name reuse"
+                           then Left $ Error "parameter name matches function name"
                            else Right pn
+
